72ef4544b8e9b944087ea4ed31077aeeddf3be04
Theis Krarup <theis0075@hotmail.com>
2023-03-17 19:44:22 +0100

    Ex16 done

---
+++ Disaheim/Disaheim/Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Controller
+    {
+        // Properties
+        public List<Book> Books { get; set; }
+        public List<Amulet> Amulets { get; set; }
+
+        // Operations
+        // Constructor
+        public Controller()
+        {
+            Books = new List<Book>();
+            Amulets = new List<Amulet>();
+        }
+
+
+        // Methods
+        // De to metoder hedder det samme, men er overloaded.
+        public void AddToList(Book book)
+        {
+            Books.Add(book);
+        }
+
+        public void AddToList(Amulet amulet)
+        {
+            Amulets.Add(amulet);
+        }
+    }
+}
